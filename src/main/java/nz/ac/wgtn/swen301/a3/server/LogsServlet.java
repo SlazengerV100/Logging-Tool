a276bdb97ffc915ad2f25ff7314f5f1f4123237c
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2024-09-18 16:59:22 +1200

    Set up skeleton for logs servlet

---
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -0,0 +1,22 @@
+package nz.ac.wgtn.swen301.a3.server;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LogsServlet {
+    LogsServlet() {
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
