c54c9ac7c2d0a55ad1bf0acd70946cb5dd9e8212
Anthony Kendrew <kendreanth@ecs.vuw.ac.nz>
2024-09-19 23:14:47 +1200

    Set up pre-conditions to throw 400 status code for /logs GET request

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,7 +1,11 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.log4j.Level;
+
+import java.io.IOException;
 
 public class LogsServlet {
     LogsServlet() {
@@ -9,7 +13,32 @@ public class LogsServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        String limitString = request.getParameter("limit");
+        String levelString = request.getParameter("level");
+        String errorDetails = "";
 
+        int limit;
+        Level level;
+
+        try {
+            limit = Integer.parseInt(limitString);
+            if (limit < 1) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                errorDetails = "'limit' must be greater than or equal to 1.";
+                return;
+            }
+        } catch (NumberFormatException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            errorDetails = "'limit' must be a valid integer.";
+            return;
+        }
+
+        if (levelString == null || (level = Level.toLevel(levelString.toUpperCase(), null)) == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            errorDetails = "'level' must be a valid log level.";
+            return;
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
