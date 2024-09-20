a47a35a6432823d9a634511efdea6617b8a0a42d
Anthony Kendrew <kendreanth@ecs.vuw.ac.nz>
2024-09-20 20:12:19 +1200

    Modify Log class to generate a random UUID for each instantiated object

--- src/main/java/nz/ac/wgtn/swen301/a3/server/Log.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Log.java
@@ -2,6 +2,27 @@ package nz.ac.wgtn.swen301.a3.server;
 
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.util.Arrays;
 import java.util.UUID;
 
-public record Log(UUID id, LoggingEvent loggingEvent, String errorDetails) {}
+public class Log {
+    private final LoggingEvent loggingEvent;
+    private final UUID id;
+
+    public Log(LoggingEvent loggingEvent) {
+        this.loggingEvent = loggingEvent;
+        id = UUID.randomUUID();
+    }
+
+    public String getId() {
+        return id.toString();
+    }
+
+    public LoggingEvent getLoggingEvent() {
+        return loggingEvent;
+    }
+
+    public String getErrorDetails() {
+        return Arrays.toString(loggingEvent.getThrowableInformation().getThrowable().getStackTrace());
+    }
+}
