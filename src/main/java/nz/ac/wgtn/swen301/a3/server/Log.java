c984275b5b4f9ce74120e4ba2e78aa5eac815c41
Anthony Kendrew <kendreanth@ecs.vuw.ac.nz>
2024-09-20 20:49:54 +1200

    Implement doGet method

--- src/main/java/nz/ac/wgtn/swen301/a3/server/Log.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Log.java
@@ -2,12 +2,15 @@ package nz.ac.wgtn.swen301.a3.server;
 
 import org.apache.log4j.spi.LoggingEvent;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.UUID;
 
 public class Log {
     private final LoggingEvent loggingEvent;
     private final UUID id;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     public Log(LoggingEvent loggingEvent) {
         this.loggingEvent = loggingEvent;
@@ -16,6 +19,26 @@ public class Log {
 
     public String getId() {
         return id.toString();
+    }
+
+    public String getName() {
+        return loggingEvent.getLoggerName();
+    }
+
+    public String getLevel() {
+        return loggingEvent.getLevel().toString().toLowerCase();
+    }
+
+    public String getTimestamp() {
+        return formatter.format(Instant.ofEpochMilli(loggingEvent.getTimeStamp()));
+    }
+
+    public String getThread() {
+        return loggingEvent.getThreadName();
+    }
+
+    public String getMessage() {
+        return loggingEvent.getRenderedMessage();
     }
 
     public LoggingEvent getLoggingEvent() {
--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,6 +1,8 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-import jakarta.servlet.ServletException;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.log4j.Level;
@@ -8,15 +10,12 @@ import org.apache.log4j.Level;
 import java.io.IOException;
 
 public class LogsServlet {
-    LogsServlet() {
+    private final Gson gson = new Gson().newBuilder().setPrettyPrinting().create();
 
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         String limitString = request.getParameter("limit");
         String levelString = request.getParameter("level");
-        String errorDetails = "";
 
         int limit;
         Level level;
@@ -25,20 +24,47 @@ public class LogsServlet {
             limit = Integer.parseInt(limitString);
             if (limit < 1) {
                 response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                errorDetails = "'limit' must be greater than or equal to 1.";
                 return;
             }
         } catch (NumberFormatException e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            errorDetails = "'limit' must be a valid integer.";
             return;
         }
 
         if (levelString == null || (level = Level.toLevel(levelString.toUpperCase(), null)) == null) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            errorDetails = "'level' must be a valid log level.";
             return;
         }
+
+        int i = Persistency.DB.size() - 1;
+        int limitCount = 0;
+        JsonArray responseBody = new JsonArray();
+
+        while (limitCount <= limit && i > 0) {
+            Log log = Persistency.DB.get(i);
+            if (log.getLoggingEvent().getLevel().isGreaterOrEqual(level)) {
+                responseBody.add(getJsonObject(log));
+                limitCount++;
+            }
+            i--;
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().write(gson.toJson(responseBody));
+    }
+
+    private static JsonObject getJsonObject(Log log) {
+        JsonObject jsonObject = new JsonObject();
+
+        jsonObject.addProperty("id", log.getId());
+        jsonObject.addProperty("name", log.getName());
+        jsonObject.addProperty("level", log.getLevel());
+        jsonObject.addProperty("timestamp", log.getTimestamp());
+        jsonObject.addProperty("thread", log.getThread());
+        jsonObject.addProperty("message", log.getMessage());
+        jsonObject.addProperty("errorDetails", log.getErrorDetails());
+
+        return jsonObject;
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
