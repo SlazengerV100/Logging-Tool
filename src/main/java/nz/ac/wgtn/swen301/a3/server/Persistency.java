f3c43cb84db40f9f2f4f06026d419da1b76ca2e3
Anthony Kendrew <kendreanth@ecs.vuw.ac.nz>
2024-09-21 13:28:47 +1200

    Make Persistency append the logger so that the DB field is populated each time a log is created

--- src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -1,7 +1,25 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
 import java.util.List;
 
-public class Persistency {
+public class Persistency extends AppenderSkeleton {
     public static List<Log> DB;
+
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
+        DB.add(new Log(loggingEvent));
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
 }
