360700035069c1045b0cb46f224586e94f72cb55
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2024-09-26 20:26:36 +1200

    Implement /stats/csv doGet

---
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/StatsCSVServlet.java
@@ -0,0 +1,88 @@
+package nz.ac.wgtn.swen301.a3.server;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatsCSVServlet extends HttpServlet {
+    private final Map<String, Map<String, Integer>> table;
+
+    public StatsCSVServlet() {
+        table = initialiseTable();
+    }
+
+    private Map<String, Map<String, Integer>> initialiseTable() {
+        Map<String, Map<String, Integer>> loggerTable = new HashMap<>();
+
+        Map<String, Integer> logger1Levels = new HashMap<>();
+        logger1Levels.put("ALL", 0);
+        logger1Levels.put("TRACE", 0);
+        logger1Levels.put("DEBUG", 3);
+        logger1Levels.put("INFO", 6);
+        logger1Levels.put("WARN", 1);
+        logger1Levels.put("ERROR", 1);
+        logger1Levels.put("FATAL", 0);
+        logger1Levels.put("OFF", 0);
+
+        Map<String, Integer> logger2Levels = new HashMap<>();
+        logger2Levels.put("ALL", 0);
+        logger2Levels.put("TRACE", 12);
+        logger2Levels.put("DEBUG", 4);
+        logger2Levels.put("INFO", 0);
+        logger2Levels.put("WARN", 0);
+        logger2Levels.put("ERROR", 1);
+        logger2Levels.put("FATAL", 0);
+        logger2Levels.put("OFF", 0);
+
+        Map<String, Integer> logger3Levels = new HashMap<>();
+        logger3Levels.put("ALL", 0);
+        logger3Levels.put("TRACE", 0);
+        logger3Levels.put("DEBUG", 0);
+        logger3Levels.put("INFO", 0);
+        logger3Levels.put("WARN", 5);
+        logger3Levels.put("ERROR", 1);
+        logger3Levels.put("FATAL", 1);
+        logger3Levels.put("OFF", 0);
+
+        loggerTable.put("logger1", logger1Levels);
+        loggerTable.put("logger2", logger2Levels);
+        loggerTable.put("logger3", logger3Levels);
+
+        return loggerTable;
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/tab-separated-values");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        out.print("Logger");
+        for (String header : table.get("logger1").keySet()) {
+            out.print("\t" + header);
+        }
+        out.println("");
+
+        for (Map.Entry<String, Map<String, Integer>> entry : table.entrySet()) {
+            String logger = entry.getKey();
+            Map<String, Integer> levels = entry.getValue();
+            out.printf("%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d%n",
+                    logger,
+                    levels.get("ALL"),
+                    levels.get("TRACE"),
+                    levels.get("DEBUG"),
+                    levels.get("INFO"),
+                    levels.get("WARN"),
+                    levels.get("ERROR"),
+                    levels.get("FATAL"),
+                    levels.get("OFF"));
+        }
+
+        out.flush();
+        out.close();
+    }
+}
