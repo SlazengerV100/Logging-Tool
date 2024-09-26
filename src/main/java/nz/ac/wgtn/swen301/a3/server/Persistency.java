472a23ebc89425e27c41a8aeaf1d7821134bfbf2
SlazengerV100 <kendreanth@ecs.vuw.ac.nz>
2024-09-26 20:59:20 +1200

    Move table initialisation to Persistency

--- src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -1,12 +1,51 @@
 package nz.ac.wgtn.swen301.a3.server;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Persistency {
     public static List<Log> DB = new ArrayList<>();
 
     public static void reset() {
         DB = new ArrayList<>();
+    }
+
+    public static Map<String, Map<String, Integer>> initialiseTable() {
+        Map<String, Map<String, Integer>> loggerTable = new TreeMap<>();
+
+        Map<String, Integer> logger1Levels = new HashMap<>();
+        logger1Levels.put("ALL", 0);
+        logger1Levels.put("TRACE", 0);
+        logger1Levels.put("DEBUG", 3);
+        logger1Levels.put("INFO", 6);
+        logger1Levels.put("WARN", 1);
+        logger1Levels.put("ERROR", 1);
+        logger1Levels.put("FATAL", 0);
+        logger1Levels.put("OFF", 0);
+
+        Map<String, Integer> logger2Levels = new HashMap<>();
+        logger2Levels.put("ALL", 0);
+        logger2Levels.put("TRACE", 12);
+        logger2Levels.put("DEBUG", 4);
+        logger2Levels.put("INFO", 0);
+        logger2Levels.put("WARN", 0);
+        logger2Levels.put("ERROR", 1);
+        logger2Levels.put("FATAL", 0);
+        logger2Levels.put("OFF", 0);
+
+        Map<String, Integer> logger3Levels = new HashMap<>();
+        logger3Levels.put("ALL", 0);
+        logger3Levels.put("TRACE", 0);
+        logger3Levels.put("DEBUG", 0);
+        logger3Levels.put("INFO", 0);
+        logger3Levels.put("WARN", 5);
+        logger3Levels.put("ERROR", 1);
+        logger3Levels.put("FATAL", 1);
+        logger3Levels.put("OFF", 0);
+
+        loggerTable.put("logger1", logger1Levels);
+        loggerTable.put("logger2", logger2Levels);
+        loggerTable.put("logger3", logger3Levels);
+
+        return loggerTable;
     }
 }
--- src/main/java/nz/ac/wgtn/swen301/a3/server/StatsCSVServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/StatsCSVServlet.java
@@ -7,55 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class StatsCSVServlet extends HttpServlet {
     private final Map<String, Map<String, Integer>> table;
 
     public StatsCSVServlet() {
-        table = initialiseTable();
-    }
-
-    private Map<String, Map<String, Integer>> initialiseTable() {
-        Map<String, Map<String, Integer>> loggerTable = new TreeMap<>();
-
-        Map<String, Integer> logger1Levels = new HashMap<>();
-        logger1Levels.put("ALL", 0);
-        logger1Levels.put("TRACE", 0);
-        logger1Levels.put("DEBUG", 3);
-        logger1Levels.put("INFO", 6);
-        logger1Levels.put("WARN", 1);
-        logger1Levels.put("ERROR", 1);
-        logger1Levels.put("FATAL", 0);
-        logger1Levels.put("OFF", 0);
-
-        Map<String, Integer> logger2Levels = new HashMap<>();
-        logger2Levels.put("ALL", 0);
-        logger2Levels.put("TRACE", 12);
-        logger2Levels.put("DEBUG", 4);
-        logger2Levels.put("INFO", 0);
-        logger2Levels.put("WARN", 0);
-        logger2Levels.put("ERROR", 1);
-        logger2Levels.put("FATAL", 0);
-        logger2Levels.put("OFF", 0);
-
-        Map<String, Integer> logger3Levels = new HashMap<>();
-        logger3Levels.put("ALL", 0);
-        logger3Levels.put("TRACE", 0);
-        logger3Levels.put("DEBUG", 0);
-        logger3Levels.put("INFO", 0);
-        logger3Levels.put("WARN", 5);
-        logger3Levels.put("ERROR", 1);
-        logger3Levels.put("FATAL", 1);
-        logger3Levels.put("OFF", 0);
-
-        loggerTable.put("logger1", logger1Levels);
-        loggerTable.put("logger2", logger2Levels);
-        loggerTable.put("logger3", logger3Levels);
-
-        return loggerTable;
+        table = Persistency.initialiseTable();
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
